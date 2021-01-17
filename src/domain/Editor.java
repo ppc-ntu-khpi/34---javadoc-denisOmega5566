ead16af9331a8d542b41f63c98e92bd13f542e01
denisOmega5566 <74858963+denisOmega5566@users.noreply.github.com>
2021-01-17 14:39:27 +0200

    Update Editor.java

--- src/domain/Editor.java
+++ src/domain/Editor.java
@@ -1,25 +1,42 @@
 package domain;
+/**
+ * Клас представляє об'єкт редактора.
+ */
 public class Editor extends Artist {
+  /**
+  * Ініціалізує новий об'єкт {@code Editor} зі всіма параметрами.
+ */
 
     public Editor(boolean electronicEditing, String[] skiils, String name, String jobTitle, int level, String dept) {
         super(skiils, name, jobTitle, level, dept);
         this.electronicEditing = electronicEditing;
     }
+ /**
+     * Створює новий об'єкт з параметром навиків редактора та електронної обробки.
+ */
 
     public Editor(boolean electronicEditing, String[] skiils) {
         super(skiils);
         this.electronicEditing = electronicEditing;
     }
-
+ /**
+     * Створює новий об'єкт з параметром електронної обробки.
+     */
     public Editor(boolean electronicEditing) {
         super();
         this.electronicEditing = electronicEditing;
     }
+    /**
+     * Створює новий об'єкт.
+     */
     
     public Editor() {
         super();
         this.electronicEditing = true;
     }
+    /**
+     * Строкове представлення об'єкта.
+     */
 
     @Override
     public String toString() {
@@ -31,13 +48,21 @@ public class Editor extends Artist {
         return s;
             
     }
+    /**
+     * Електронна обробка
+     */
 
     private boolean electronicEditing;
+    /**
+     * Повертає перевагу редактора.
+     */
 
     public boolean getPreferences() {
         return electronicEditing;
     }
-
+    /**
+     * Встановлює поле електронної обробки. 
+     */
     public void setPreferences(boolean electronic) {
         this.electronicEditing=electronic;
     }
